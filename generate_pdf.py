4f2b9480be5f2ca1ca3f0298e01f3f2d69eeaa17
DivyanshuSahu <dsahu1997@gmail.com>
2018-11-08 14:02:41 +0530

    generate pdf from text file

---
+++ generate_pdf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import pdfkit
+import sys
+
+def main() :
+
+	if len(sys.argv) != 2 :
+		print("./generate_pdf.py <filenane>")
+		#sys.exit()
+
+	filename = sys.argv[1]
+	with open(filename, 'rb') as f :
+		text = f.read()
+	text = text.decode("utf-8")
+	#print(text)
+
+	replace_dict = {'\n':'<br>'}
+	data = text.replace('\n','<br>')
+	
+	options = {
+		'page-size' : 'Letter',
+		'margin-top' : '0.75in',
+		'margin-right' : '0.75in',
+		'margin-bottom' : '0.75in',
+		'margin-left' : '0.75in',
+		'encoding' : "UTF-8",
+		'footer-center' : '[page] of [topage]'
+	}
+
+	newfile_name = filename.replace('.txt','.pdf')
+	pdfkit.from_string(data, newfile_name, options=options)
+
+if __name__ == '__main__':
+	main()
