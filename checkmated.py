2b261723ccd31ba2159d64195a7c12305e8242e7
Divyanshu Sahu <dsahu1997@gmail.com>
2019-02-22 04:52:51 +0530

    checkmated archieve checker added

---
+++ checkmated.py
@@ -0,0 +1,22 @@
+import requests
+
+"""for i in range(1,10000) :
+	url = "https://web.archive.org/web/submit?url=http://checkmated.com/story.php?story=%s&type=replay" % (str(i))
+	r = requests.get(url)
+	if r.status_code == 200 :
+		print i"""
+
+i = 1
+while True :
+	
+	url = "https://web.archive.org/web/submit?url=http://checkmated.com/story.php?story=%s&type=replay" % (str(i))
+	r = requests.get(url)
+	if r.status_code == 200 :
+		print i
+
+	if i % 10 == 0 :
+		c = raw_input("Continue(Y/N): ")
+		if c.lower() == "n" :
+			break
+	
+	i += 1
